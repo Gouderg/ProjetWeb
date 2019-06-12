2c27b1696f5ce4a75feb2992ee71d54e32d2f774
villie23 <victor.illien@isen-ouest.yncrea.fr>
2019-06-12 14:13:51 +0200

    Version 1.3

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+var type, ville,url;
+
+function temp() {
+	ville = document.getElementById("ville").value;
+	type = document.getElementById("type").value;
+	url = "http://ville.api.isen-ouest.fr/immo/list/ville/"+ville+"/type/"+type;
+	location.href="rechercheGlobal.html";
+	
+	miseajour(url);
+	
+}
+
+function clearTableau(){
+	var tbody = document.getElementById('tableBody');
+
+	while (tbody.firstChild) {
+		tbody.removeChild(tbody.firstChild);
+	}
+
+}
+
+
+function recup(json) {
+	console.log("lol");
+	console.log(json);
+	clearTableau();
+
+}
+
+
+function miseajour(url) {
+
+	console.log("MAJ" + url);
+
+	fetch(url, {
+		method : 'GET'
+	})
+	.then( (res) => res.json())
+	.then( (json) => recup(json))
+
+	console.log("bonswar");
+
+}
+
+
+
+
+
+
+/*//Code Js pour le swipe dans Recherche précise
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,4 +76,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   console.log(slideIndex)
-} 
+} */
